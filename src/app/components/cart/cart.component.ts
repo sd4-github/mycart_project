613b3e0ebce0b3329ee8595f91c50b935ffc3255
sd4 <soumikd4@gmail.com>
2020-05-15 12:49:25 +0530

    add to cart button

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -13,11 +13,24 @@ export interface Transaction {
 })
 export class CartComponent implements OnInit {
   cart:any=[];
+  quantity: number = 1;
   constructor(private cartSrvc:CartService) { }
 
   ngOnInit() {
     this.cart=this.cartSrvc.getItems();
     // console.log(this.cart);
+  }
+
+  increment() {
+    this.quantity += 1;
+  }
+
+  decrement() {
+    if (this.quantity!=1) {
+      this.quantity -= 1;
+    } else {
+      this.quantity=1;
+    }
   }
 
 }
