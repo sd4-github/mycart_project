84825b881533eace8a3a5fee6b5a686be948bc52
sd4 <soumikd4@gmail.com>
2020-08-19 23:31:11 +0530

    heroku deploy setup

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-
-app.use(express.static('./dist/mycart'));
-
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/mycart/'});
+app.use(express.static(__dirname + '/dist/mycart'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname +
+    '/dist/mycart/index.html'));
 });
-
 app.listen(process.env.PORT || 8080);
-
