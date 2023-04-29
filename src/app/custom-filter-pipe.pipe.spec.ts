f295e04f267e74fda7a7608093419d9d1313324e
sd4-github <soumikd4@gmail.com>
2023-04-30 01:04:20 +0530

    node version package.json custom-pipe #44

---
+++ src/app/custom-filter-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CustomFilterPipe } from './custom-filter-pipe.pipe';
+
+describe('CustomFilterPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new CustomFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
